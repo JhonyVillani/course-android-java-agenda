968ae53de0984adfdcbbe121595e8c2f14937822
Jhony <jhony.v.m@hotmail.com>
2019-11-29 10:11:41 -0300

    Criando lista com TextView

--- app/src/main/java/br/com/jhonyvillani/MainActivity.java
+++ app/src/main/java/br/com/jhonyvillani/MainActivity.java
@@ -2,16 +2,28 @@ package br.com.jhonyvillani;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Pedro", "Jhean", "Henrique"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
