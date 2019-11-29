d4ec329f659d0bcf0169cd081c6b5ae723a73e8b
Jhony <jhony.v.m@hotmail.com>
2019-11-29 15:57:31 -0300

    Utilizando AdapterViews para colecoes de views

--- app/src/main/java/br/com/jhonyvillani/MainActivity.java
+++ app/src/main/java/br/com/jhonyvillani/MainActivity.java
@@ -1,29 +1,30 @@
 package br.com.jhonyvillani;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Pedro", "Jhean", "Henrique"));
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+                Arrays.asList("Pedro", "Jhean", "Henrique", "Jhony"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
     }
 }
