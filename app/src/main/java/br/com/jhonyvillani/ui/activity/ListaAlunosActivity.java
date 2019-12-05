fe039042936e9f7fe0bed7f7f8697aa7c051b351
Jhony <jhony.v.m@hotmail.com>
2019-12-05 08:53:41 -0300

    Refatorando o codigo

--- app/src/main/java/br/com/jhonyvillani/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/jhonyvillani/ui/activity/ListaAlunosActivity.java
@@ -16,6 +16,9 @@ import br.com.jhonyvillani.dao.AlunoDAO;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Lista de alunos";
+    private final AlunoDAO dao = new AlunoDAO();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,24 +26,33 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
         AlunoDAO dao = new AlunoDAO();
 
-        setTitle("Lista de alunos");
+        setTitle(TITULO_APPBAR);
+        configuraFabNovoAluno();
+    }
 
+    private void configuraFabNovoAluno() {
         FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
         botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ListaAlunosActivity.this,
-                        FormularioAlunoActivity.class));
+                abreFormularioAlunoActivity();
             }
         });
+    }
+
+    private void abreFormularioAlunoActivity() {
+        startActivity(new Intent(this, FormularioAlunoActivity.class));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        AlunoDAO dao = new AlunoDAO();
 
+        configuraLista();
+    }
+
+    private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
