9b7d5565674310574043249cd15836c692bec3d0
Jhony <jhony.v.m@hotmail.com>
2019-12-11 14:47:26 -0300

    Removendo aluno no DAO

--- app/src/main/java/br/com/jhonyvillani/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/jhonyvillani/ui/activity/ListaAlunosActivity.java
@@ -2,6 +2,7 @@ package br.com.jhonyvillani.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -60,6 +61,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
         final List<Aluno> alunos = dao.todos();
         configuraAdapter(listaDeAlunos, alunos);
         configuraListenerDeCliquePorItem(listaDeAlunos);
+        listaDeAlunos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Aluno alunoEscolhido = (Aluno) adapterView.getItemAtPosition(posicao);
+                dao.remove(alunoEscolhido);
+                return true;
+            }
+        });
     }
 
     private void configuraListenerDeCliquePorItem(ListView listaDeAlunos) {
