2b0c36c246e34da8d5bca10742034698872ada8e
Jhony <jhony.v.m@hotmail.com>
2019-12-18 09:56:23 -0300

    Apresentando informacoes do aluno na View

--- app/src/main/java/br/com/jhonyvillani/ui/adapter/ListaAlunosAdapter.java
+++ app/src/main/java/br/com/jhonyvillani/ui/adapter/ListaAlunosAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,10 +38,15 @@ public class ListaAlunosAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(int posicao, View view, ViewGroup viewGroup) {
         View viewCriada = LayoutInflater
                 .from(context)
                 .inflate(R.layout.item_aluno, viewGroup, false);
+        Aluno alunoDevolvido = alunos.get(posicao);
+        TextView nome = viewCriada.findViewById(R.id.item_aluno_nome);
+        nome.setText(alunoDevolvido.getNome());
+        TextView telefone = viewCriada.findViewById(R.id.item_aluno_telefone);
+        telefone.setText(alunoDevolvido.getTelefone());
         return viewCriada;
     }
 
