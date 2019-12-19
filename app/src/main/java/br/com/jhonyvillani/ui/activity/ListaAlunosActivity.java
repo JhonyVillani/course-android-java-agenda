bf63b7e991b5765e836d9276c8e82c7370ce8fb1
Jhony <jhony.v.m@hotmail.com>
2019-12-19 11:06:49 -0300

    Implementando o AlertDialog

--- app/src/main/java/br/com/jhonyvillani/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/jhonyvillani/ui/activity/ListaAlunosActivity.java
@@ -1,5 +1,6 @@
 package br.com.jhonyvillani.ui.activity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -34,6 +35,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
         configuraLista();
+        new AlertDialog
+                .Builder(this)
+                .setTitle("Removendo aluno")
+                .setMessage("Tem certeza que quer remover o aluno?")
+                .setPositiveButton("Sim", null)
+                .setNegativeButton("Não", null)
+                .show();
+
     }
 
     @Override
